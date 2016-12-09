136b34ce0d38953256b2d489b3e312c4305eafbe
Gabriel Malaquias <gabriel07malakias@gmail.com>
2016-12-08 23:51:04 -0200

    Adds a custom notifier

--- config/initializers/stoplight.rb
+++ config/initializers/stoplight.rb
@@ -4,3 +4,4 @@ require 'redis'
 redis = Redis.new(host: ENV["REDIS"])
 
 Stoplight::Light.default_data_store = Stoplight::DataStore::Redis.new(redis)
+Stoplight::Light.default_notifiers += Array(Notifiers::Custom.new)
